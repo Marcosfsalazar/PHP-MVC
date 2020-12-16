a69e199edd3578ae9f53481e9b24f1cf5e17dcfc
Marcos Salazar <Marcos1010521@gmail.com>
2020-12-16 16:13:42 -0400

    abstraçao classe routes / models sendo chamadas pelos controllers

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,12 +4,25 @@ namespace App\Controllers;
 
 class IndexController{
 
+    private $view;
+
+    public function __construct(){
+        $this->view = new \stdClass();
+    }
+
     public function index(){
-        echo "chegamos ao IndexController action index";
+        $this->render("index");
     }
 
     public function sobreNos(){
-        echo "chegamos ao IndexController action sobreNos" ;
+        $this->render("sobre_nos");
+    }
+
+    public function render($view){
+        $class = get_class();
+        $class = str_replace('App\\Controllers\\','',$class);
+        $class = strtolower(str_replace('Controller','',$class));
+        require_once("/var/www/html/miniframework/App/Views/".$class."/".$view.".phtml");
     }
 }
 
--- public/index.php
+++ public/index.php
@@ -4,8 +4,4 @@ require_once "../vendor/autoload.php";
 
 $route = new \App\Route;
 
-echo "It's working!";
-
-print_r($route->getUrl());
-
 ?>
